47824d45679eca594f0edfb408212867e6f49486
Mariya A F <mariyaaf98@gmail.com>
2025-08-01 20:28:35 +0530

    final commit

--- todo_project/todo/urls.py
+++ todo_project/todo/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("add/",views.taskList,name="task_list"),
     path('',views.addTask,name='add_task'),
-
+    path('complete/<int:pk>',views.completeTask,name="complete_task"),
+    path('edit/<int:pk>',views.editTask,name="edit_task"),
+    path('delete/<int:pk>',views.deleteTask,name="delete_task")
 
 ]
--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from . models import Task 
 
 def addTask(request):
@@ -13,3 +13,30 @@ def addTask(request):
 def taskList(request):
     tasks=Task.objects.all().order_by('created_at')
     return render(request,'todo_list.html',{'tasks':tasks})
+
+
+def completeTask(request,pk):
+    task=get_object_or_404(Task,id=pk)
+    task.completed=not task.completed
+    task.save()
+    return redirect('task_list')
+
+
+def deleteTask(request,pk):
+    task=get_object_or_404(Task,id=pk)
+    task.delete()
+    return redirect('task_list')
+
+def editTask(request, pk):
+    task = get_object_or_404(Task, id=pk)
+    tasks = Task.objects.all() 
+
+    if request.method == "POST":
+        task.title = request.POST.get('title')
+        task.save()
+        return redirect('task_list') 
+
+    return render(request, 'todo_list.html', {'tasks': tasks, 'edit_task': task})
+
+
+
